8304530f26480a6cc8e0a1314e5a5c9ed2676ae4
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-28 16:56:04 +0700

    perbaikan

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default function AboutScreen(){
-    return(
-        <view style={tyles.container}>
-            <text style={tyles.title}>Aplikasi</text>
-            <text style={tyles.description}>
+export default function AboutScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Aplikasi</Text>
+            <Text style={styles.description}>
                 Ini Adalah Daftar Makanan All Star dan Resep 🍽️
-            </text>
-        </view>
+            </Text>
+        </View>
     );
 }
 
-const style= StyleSheet.create ({
+const styles = StyleSheet.create({
     container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
-    title: {fontSize: 24, fontWeight: 'bold', marginBottom: 20},
-    description: { fontSize: 16, textAlign: 'center', paddingHorizontal: 30},
+    title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+    description: { fontSize: 16, textAlign: 'center', paddingHorizontal: 30 },
 });
