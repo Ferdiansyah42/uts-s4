130eee793004e6782e1d0fc977ec996a48a52343
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-28 15:09:32 +0700

    route

--- App.js
+++ App.js
@@ -16,3 +16,31 @@ function HomeStack() {
     </Stack.Navigator>
   );
 }
+
+export default function App (){
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home'){
+            iconName = 'home';
+          }
+          else if (route.name === 'About'){
+            iconName = 'information-circle';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#f39c12',
+        tabBarInactiveTintColor: 'gray',
+      })}
+      >
+        <Tab.Screen name="Home" component={HomeStack} options={{ headerShown: false }} />
+        <Tab.Screen name="About" component={AboutScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
