e5caebe5a025958a2d40cf883a482e0e92bcaac2
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-28 17:09:56 +0700

    di rapikan

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,18 +2,18 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function AboutScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Aplikasi</Text>
-            <Text style={styles.description}>
-                Ini Adalah Daftar Makanan All Star dan Resep 🍽️
-            </Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Aplikasi</Text>
+      <Text style={styles.description}>
+        Ini Adalah Daftar Makanan All Star dan Resep 🍽️
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
-    title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
-    description: { fontSize: 16, textAlign: 'center', paddingHorizontal: 30 },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
+  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+  description: { fontSize: 16, textAlign: 'center', paddingHorizontal: 30 },
 });
