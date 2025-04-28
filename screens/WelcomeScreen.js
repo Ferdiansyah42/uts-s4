1f916660b485451f7974df9af364850a0627d5d4
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-28 19:35:13 +0700

    memperbaiki huruf besar kecilnya

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,56 +1,59 @@
-import React from "react";
+import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
 
-export default function welcomeScreen ({ navigation }){
-    return (
-        <ImageBackground source={require('../assets/background.png')} 
-        style={StyleSheet.background}>
-            <view>
-                <Text> Selamat Datang </Text>
-                <Text> Temukan Resep Favorit Kamu! </Text>
+export default function WelcomeScreen({ navigation }) {
+  return (
+    <ImageBackground
+      source={require('../assets/background.jpg')}
+      style={styles.background}
+    >
+      <View style={styles.overlay}>
+        <Text style={styles.title}>Selamat Datang</Text>
+        <Text style={styles.subtitle}>Temukan Resep Favoritmu!</Text>
 
-                <TouchableOpacity style={StyleSheet.button}
-                onPress={() => navigation.replace ('HomeTab')}>
-
-                    <Text style= {StyleSheet.button}> Get Started </Text>
-                </TouchableOpacity>
-            </view>
-        </ImageBackground>
-    );
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.replace('HomeTab')}
+        >
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    overlay: {
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        padding: 30,
-        borderRadius: 20,
-        alignItems: 'center',
-    },
-    title: {
-        color: 'wihte',
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    subtitle: {
-        color: 'white',
-        fontSize: 16,
-        marginBottom: 20,
-        textAlign: 'center',
-      },
-      button: {
-        backgroundColor: '#f39c12',
-        paddingVertical: 12,
-        paddingHorizontal: 25,
-        borderRadius: 30,
-      },
-      buttonText: {
-        color: 'white',
-        fontSize: 18,
-      },
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 30,
+    borderRadius: 20,
+    alignItems: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: 'white',
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#f39c12',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 30,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
 });
