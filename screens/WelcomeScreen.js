2ed6107ad5a10af91f7e8ce64520f107f4e229d8
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-28 19:33:30 +0700

    styling

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -11,10 +11,46 @@ export default function welcomeScreen ({ navigation }){
 
                 <TouchableOpacity style={StyleSheet.button}
                 onPress={() => navigation.replace ('HomeTab')}>
-                    
+
                     <Text style= {StyleSheet.button}> Get Started </Text>
                 </TouchableOpacity>
             </view>
         </ImageBackground>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    overlay: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        padding: 30,
+        borderRadius: 20,
+        alignItems: 'center',
+    },
+    title: {
+        color: 'wihte',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    subtitle: {
+        color: 'white',
+        fontSize: 16,
+        marginBottom: 20,
+        textAlign: 'center',
+      },
+      button: {
+        backgroundColor: '#f39c12',
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        borderRadius: 30,
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 18,
+      },
+});
