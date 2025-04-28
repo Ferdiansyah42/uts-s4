527c86a4d094482fa0cc249cfd997311d261ec48
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-28 17:13:04 +0700

    diperbaiki dan di rapikan

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,28 +1,29 @@
+// DetailScreen.js
 import React from 'react';
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 
 export default function DetailScreen({ route }) {
-    const { recipe } = route.params;
+  const { recipe } = route.params;
 
-    return (
-        <ScrollView style={styles.container}>
-            <Image source={{ uri: recipe.strMealThumb }} style={styles.image} />
-            <Text style={styles.title}>{recipe.strMeal}</Text>
-            <Text style={styles.origin}>Asal: {recipe.strArea}</Text>
+  return (
+    <ScrollView style={styles.container}>
+      <Image source={{ uri: recipe.strMealThumb }} style={styles.image} />
+      <Text style={styles.title}>{recipe.strMeal}</Text>
+      <Text style={styles.origin}>Asal: {recipe.strArea}</Text>
 
-            <Text style={styles.subtitle}>Instruksi Memasak:</Text>
-            <Text style={styles.instructions}>
-            {recipe.strInstructions}
-            </Text>
-        </ScrollView>
-    );
+      <Text style={styles.subtitle}>Instruksi Memasak:</Text>
+      <Text style={styles.instructions}>
+        {recipe.strInstructions}
+      </Text>
+    </ScrollView>
+  );
 }
 
-const style = StyleSheet.create ({
-    container: {flex: 1, backgroundColor: '#fff'},
-    image: {width: '100%', height: 250 },
-    title: { fontSize: 24, fontWeight: 'bold', marginVertical: 10, textAlign: 'center'},
-    origin: {fontSize: 16, marginBottom: 10, textAlign: 'center'},
-    subtitle: { fontSize: 18, fontWeight: 'bold', marginHorizontal: 16, marginBottom: 8},
-    instructions: {fontSize: 16, marginHorizontal: 16, lineHeight: 22},
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#fff' },
+  image: { width: '100%', height: 250 },
+  title: { fontSize: 24, fontWeight: 'bold', marginVertical: 10, textAlign: 'center' },
+  origin: { fontSize: 16, marginBottom: 10, textAlign: 'center' },
+  subtitle: { fontSize: 18, fontWeight: 'bold', marginHorizontal: 16, marginBottom: 8 },
+  instructions: { fontSize: 16, marginHorizontal: 16, lineHeight: 22 },
 });
